5cc84cb38ef77e63111e96a5b4b4d7b6f3448238
Nguyễn Ngọc Lãm <35496264+panasoniclam@users.noreply.github.com>
2020-11-24 17:17:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,11 @@
-const express = require('express');
-const app = express();
+const express = require('express'); 
+const app = express(); 
+const port = 3000; 
 
-
-app.use(express.static("public"));
-app.set("view engine","ejs");
-app.set("views","./views");
-
-
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
-server.listen(3000);
-
-io.on("connection",function(socket){
-    console.log("co nguoi ket noi",socket.id);
-    socket.on("disconnect",function(){
-        console.log(socket.id,"ngat ket noi")
-    })
-    socket.on("client-send-data",function(data){
-        console.log(data);
-        io.sockets.emit("server-send-data",data+"888")
-    })
+app.get('/', function(req, res){
+    res.send("Hello World");
 })
-app.get('/',(rep,res)=>{
-    res.render("trangchu");
-});
+
+app.listen(port, function(){
+    console.log("Your app running on port " + port);
+})
